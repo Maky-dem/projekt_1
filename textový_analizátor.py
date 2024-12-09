cd3343b6e863c1fc527cf5b6cac13adb754c4c6a
Maky-dem <demura.m@seznam.cz>
2024-12-09 11:13:05 +0100

    Update textový_analizátor.py

--- textový_analizátor.py
+++ textový_analizátor.py
@@ -2,22 +2,20 @@
     projekt_1.py: první projekt do Engento Online Python Akademie
     autor: Markéta Fejtek
     email: demura.m@seznam.cz
+
 """
+import collections
+
+import re
+
 registrovane_uzivatele = {
     "bob": "123", 
     "ann": "pass123", 
     "mike": "password123",
     "liz": "pass123"}
+
 odelovac = "_ " * 30
-prihlasovaci_jmeno = input(str("username:"))
-heslo = input(str("password:")) 
-print(odelovac)
-if prihlasovaci_jmeno in registrovane_uzivatele and\
-    registrovane_uzivatele[prihlasovaci_jmeno] == heslo:
-    print("Welcome to the app, ", prihlasovaci_jmeno)
-else:
-    print("Unregistered user, terminating the programm")
-    quit()
+
 TEXTS: list[str] = ['''
 Situated about 10 miles west of Kemmerer,
 Fossil Butte is a ruggedly impressive
@@ -45,48 +43,75 @@ other freshwater genera and herring similar to those
 in modern oceans. Other fish such as paddlefish,
 garpike and stingray are also present.'''
 ]
+
+prihlasovaci_jmeno = input(str("username:"))
+heslo = input(str("password:")) 
+print(odelovac)
+
+if prihlasovaci_jmeno in registrovane_uzivatele and\
+    registrovane_uzivatele[prihlasovaci_jmeno] == heslo:
+    print("Welcome to the app, ", prihlasovaci_jmeno)
+else:
+    print("Unregistered user, terminating the programm")
+    quit()
+
 print("We have", len(TEXTS), "texts to be analyzed.")
 print(odelovac)
-text_k_analyza = input(("Enter a number btw. 1 and "+\
-         str(len(TEXTS))+ ":"))
-if text_k_analyza.isdigit() and int(text_k_analyza)\
-     in range(1, len(TEXTS) + 1):
-    print("Analysis in progress, text number :", text_k_analyza)
+
+text_k_analyza = input("Enter a number btw. 1 and "+\
+         str(len(TEXTS)) + "to select :")
+if text_k_analyza:
+    text_k_analyza.isdigit() and int(text_k_analyza)\
+     in range(1, len(TEXTS) + 1)
+    
 else:
     print("You have entered a number that we do not have in our offer,\
          terminating the programm")
     quit()
+
 text_analizuji = TEXTS[int(text_k_analyza)-1]
-slova_textu = text_analizuji.split()# rozdeleni textu na slova
-print(odelovac) # rozdeleni textu na slova
+cleand_text = re.sub(r"[^a-zA-z0-9\s]", " ", text_analizuji)
+slova_textu = cleand_text.split()# rozdeleni textu na slova
+print(odelovac) 
 print("There are", len(slova_textu), "words in the selected text.")
-pocet_slov_zvp = sum( 1 for slovo in slova_textu if slovo.istitle() \
-    and slovo.isalpha()) # počet slov z velkych písmen
+
+pocet_slov_zvp = 0
+pocet_slov_vp = 0
+pocet_slov_vp_mal = 0
+pocet_cisel = 0
+suma_vsech_cisel_v_textu = 0
+
+for slovo in slova_textu:
+    if slovo in slova_textu and slovo.istitle() and slovo.isalpha():
+        pocet_slov_zvp += 1 # počet slov z velkych písmen
+    if slovo.isupper() and slovo.isalpha():
+        pocet_slov_vp += 1  # počet slov psaných velkými písmeny
+    if slovo.islower() and slovo.isalpha():
+        pocet_slov_vp_mal += 1 # počet slov psaných malými písmeny
+    if slovo.isdigit():
+        pocet_cisel += 1 # počet čísel v textu
+        suma_vsech_cisel_v_textu += int(slovo)
+
 print("There are", pocet_slov_zvp, "titlecase words.")
-pocet_slov_vp = sum( 1 for slovo in slova_textu if slovo.isupper() \
-    and slovo.isalpha()) # počet slov psaných velkými písmeny
 print("There are", pocet_slov_vp,"uppercase words.")
-pocet_slov_vp_mal = sum( 1 for slovo in slova_textu if slovo.islower() \
-    and slovo.isalpha()) # počet slov psaných malými písmeny
 print("There are", pocet_slov_vp_mal, "lowercase words.")
-pocet_cisel = sum(1 for slovo in slova_textu if slovo.isdigit()) 
-# počet čísel v textu
 print("There are", pocet_cisel, "numeric strings.")
-suma_vsech_cisel_v_textu = sum(int(slovo) for slovo in slova_textu\
-     if slovo.isdigit()) # součet všech čísel v textu
 print("The sum of all numbers", suma_vsech_cisel_v_textu)
-import collections
-def analizuj_delky_slov(slova_textu):
+
+def analizuj_delky_slov(text):
     delky_slov = []
-    for slovo in text_analizuji.split():
+    for slovo in slova_textu:
         delky_slov.append(len(slovo))
     pocet_slov_dle_delky = collections.Counter(delky_slov)
     return delky_slov, pocet_slov_dle_delky
-delky_slov, pocet_slov_dle_delky = analizuj_delky_slov(text_analizuji) 
+    
+delky_slov, pocet_slov_dle_delky = analizuj_delky_slov(slova_textu) 
+
 odstup_tabulka = int(max(pocet_slov_dle_delky.values())) + 1  
 print(odelovac) 
 print("LEN| OCCURENCES", "|NR.", sep=" "*(odstup_tabulka-11))  
 print(odelovac)
+
 for length, occurrences in sorted(pocet_slov_dle_delky.items()):
     print(f"{length:3}| {'*' * occurrences:<{odstup_tabulka-1}}|{
         occurrences}".ljust(odstup_tabulka+3))
